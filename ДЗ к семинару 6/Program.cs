b993cedc3b43a5033eabfae52e5358fea8d56481
YuraPyur <yupyur1991@mail.ru>
2022-11-30 14:25:05 +0300

    dopolnil DZ k seminaru 6

--- ДЗ к семинару 6/Program.cs
+++ ДЗ к семинару 6/Program.cs
@@ -26,22 +26,30 @@
 // b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
 
 
-Console.WriteLine("Введите b1");
-double b1 = double.Parse(Console.ReadLine());
-Console.WriteLine("Введите k1");
-double k1 = double.Parse(Console.ReadLine());
-Console.WriteLine("Введите b2");
-double b2 = double.Parse(Console.ReadLine());
-Console.WriteLine("Введите k2");
-double k2 = double.Parse(Console.ReadLine());
-
-double x = -1000;
-if (k1*x + b1 == k2*x + b2)
-{
-    double y = k1*x + b1;
-    Console.WriteLine(x + " " + y);
-}
-else 
-{ 
-    x = x + 0.0001;
-}
+// Console.WriteLine("Введите b1");
+// double b1 = double.Parse(Console.ReadLine());
+// Console.WriteLine("Введите k1");
+// double k1 = double.Parse(Console.ReadLine());
+// Console.WriteLine("Введите b2");
+// double b2 = double.Parse(Console.ReadLine());
+// Console.WriteLine("Введите k2");
+// double k2 = double.Parse(Console.ReadLine());
+// double x = 0;
+// double y = 0;
+// if (k1 == k2)
+// {
+//     if (b1 == b2)
+//     {
+//         Console.WriteLine("Прямые совпадают");
+//     }
+//     else
+//     {
+//         Console.WriteLine("Прямые не пересекаются");
+//     }
+// } 
+// else
+// {
+//     x = (b2-b1)/(k1-k2);
+//     y = k1*x+b1;
+//     Console.WriteLine($"Координаты точки пересечения - ({x};{y})");
+// }
