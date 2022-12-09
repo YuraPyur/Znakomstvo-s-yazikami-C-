c8dc2ef2a8618041b1e59dfe8db45b6fa8a1d2ac
YuraPyur <yupyur1991@mail.ru>
2022-12-09 23:49:39 +0300

    add Seminar 9 and DZ k seminaru 9

---
+++ ДЗ к семинару 9/Program.cs
@@ -0,0 +1,64 @@
+﻿// Задача 64: Задайте значение N. Напишите программу, которая выведет 
+// все натуральные числа в промежутке от N до 1. Выполнить с помощью рекурсии.
+
+// N = 5 -> "5, 4, 3, 2, 1"
+// N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1"
+
+// Console.WriteLine("введите число");
+// int m = int.Parse(Console.ReadLine());
+// Console.WriteLine($"N = {m} -> " + $"{Sum(m)}");
+
+// string Sum(int number)
+// {
+
+//     if (number == 1) return "1 ";
+//     return $"{number} " + $"{Sum(number - 1)}";
+// }
+
+
+
+// Задача 66: Задайте значения M и N. Напишите программу, которая найдёт 
+// сумму натуральных элементов в промежутке от M до N.
+
+// M = 1; N = 15 -> 120
+// M = 4; N = 8. -> 30
+
+// Console.WriteLine("Начало диапозона");
+// int e = int.Parse(Console.ReadLine());
+// Console.WriteLine("Конец диапозона");
+// int m = int.Parse(Console.ReadLine());
+// Console.WriteLine($"Сумма натуральных чисел от {e} до {m} равна {Sum(m, e)}");
+
+// int Sum(int number, int end)
+// {
+
+//     if (number == end) return end;
+//     return number + Sum(number - 1, end);
+// }
+
+
+
+// Задача 68: Напишите программу вычисления функции Аккермана с 
+// помощью рекурсии. Даны два неотрицательных числа m и n.
+// m = 2, n = 3 -> A(m,n) = 9
+// m = 3, n = 2 -> A(m,n) = 29
+
+// Console.WriteLine("Первое неотрицательное число");
+// int e = int.Parse(Console.ReadLine());
+// int e1 = 0;
+// if (e>0) e1=e;
+// else Console.WriteLine($"Число {e} - отрицательное"); 
+// Console.WriteLine("Второе неотрицательное число");
+// int second = int.Parse(Console.ReadLine());
+// int second2=0;
+// if (second>0) second2=second;
+// else Console.WriteLine($"Число {second} - отрицательное");
+// Console.WriteLine($"Функция Аккермана: А({e1},{second2}) = {Sum(e1,second2)}");
+
+// int Sum(int n, int m)
+// {
+//     if (n==0) return m+1;
+//     else if (n>0 && m==0) return Sum(n-1,1);
+//     return Sum(n-1,Sum(n,m-1));
+// }
+
---
+++ Семинар 9/Program.cs
@@ -0,0 +1,44 @@
+﻿
+// Console.WriteLine("введите число");
+// int m = int.Parse(Console.ReadLine());
+// Console.WriteLine($"{m} -> {Sum(m)}");
+// int Sum(int number)
+// {
+//     if (number==0) return 0;
+//     return (number%10 + Sum(number/10));
+// }
+
+// Задача 1. Задайте значение N. Напишите программу, которая выведет
+//  все натуральные числа в промежутке от 1 до N.
+
+// N = 5 -> "1, 2, 3, 4, 5"
+// N = 6 -> "1, 2, 3, 4, 5, 6"
+
+// Console.WriteLine("введите число");
+// int m = int.Parse(Console.ReadLine());
+// Console.WriteLine(Sum(m));
+
+// string Sum(int number)
+// {
+    
+//     if (number ==1) return "1 ";
+//     return $"{Sum(number-1)}" + $"{number} ";
+// }
+
+
+// Задача 2. Напишите программу, которая на вход принимает два числа 
+// A и B, и возводит число А в целую степень B с помощью рекурсии.
+
+// A = 3; B = 5 -> 243 (3⁵)
+// A = 2; B = 3 -> 8
+
+// int IntPow(int a, int b)
+// {
+//     if (b==0) return 1;
+//     return IntPow(a,b-1)*a;
+// }
+// Console.WriteLine("Введите значение А");
+// int a = int.Parse(Console.ReadLine());
+// Console.WriteLine("Введите значение B");
+// int b = int.Parse(Console.ReadLine());
+// Console.WriteLine(IntPow(a,b));
